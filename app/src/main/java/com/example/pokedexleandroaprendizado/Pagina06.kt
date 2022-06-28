ab5aec16277fa2edbca03f5ef740e459fc3f0c2d
jose eugenio <eugeniojsocio75@gmail.com>
2022-06-27 21:14:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pokedexleandroaprendizado/Pagina06.kt
@@ -0,0 +1,11 @@
+package com.example.pokedexleandroaprendizado
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Pagina06 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina06)
+    }
+}
